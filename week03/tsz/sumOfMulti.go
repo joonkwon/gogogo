a729f317bf6bc6a730fd81acfdbf1f926ad21cdc
Tones <tonyshuzhang@gmail.com>
2019-05-19 20:10:59 -0400

    Update sumOfMulti.go

--- week03/tsz/sumOfMulti.go
+++ week03/tsz/sumOfMulti.go
@@ -2,18 +2,36 @@ package main
 
 import "fmt"
 
+type IntSet struct {
+	set map[int]bool
+}
+
+func (intset *IntSet) add(element int) {
+	intset.set[element] = true
+}
+
+func (intset *IntSet) getSum() int {
+	sum := 0
+	for element, _ := range intset.set {
+		sum += element
+	}
+	return sum
+}
+
 func SumOfMultiples(limit int, divisors []int) int {
-	totalSum := 0
+
+	set := &IntSet{make(map[int]bool)}
+
 	for _, divisor := range divisors {
 		multiple := divisor
 		for multiple < limit {
-			totalSum += multiple
+			set.add(multiple)
 			multiple += divisor
 		}
 
 	}
 
-	return totalSum
+	return set.getSum()
 }
 
 func main() {
@@ -22,5 +40,5 @@ func main() {
 	fmt.Printf("Sum of multiples is %d\n", SumOfMultiples(15, []int{3, 4, 5}))
 	fmt.Printf("Sum of multiples is %d\n", SumOfMultiples(0, []int{3, 4}))
 	fmt.Printf("Sum of multiples is %d\n", SumOfMultiples(5, []int{}))
-
+	fmt.Printf("Sum of multiples is %d\n", SumOfMultiples(20, []int{3, 4, 5, 5, 6}))
 }
